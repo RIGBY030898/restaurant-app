9ed007a1a99fd6d422bab5568a229a794fc09be2
Jesús Jimenez <jesusjimeneztapia456@gmail.com>
2020-07-10 14:51:21 -0400

    Refactoring of services.

--- Web/src/service/firebase/index.js
+++ Web/src/service/firebase/index.js
@@ -1,14 +1,12 @@
-import history from './history'
 import {
     databaseUserRestaurant,
     getUserRestuarant,
     registerUserRestaurant,
     referenceUser,
     referenceUserRestaurant,
-} from './firebase'
+} from './serviceUser'
 
 export {
-    history,
     databaseUserRestaurant,
     getUserRestuarant,
     registerUserRestaurant,
